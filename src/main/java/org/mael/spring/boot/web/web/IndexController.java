01c2e309a6ae35b88736bbc29b98a4bb48070815
ElderMael <sftwr.mael@gmail.com>
2014-04-26 16:26:31 -0500

    Basic Spring Boot + Thymeleaf Project Skeleton

---
+++ src/main/java/org/mael/spring/boot/web/web/IndexController.java
@@ -0,0 +1,28 @@
+package org.mael.spring.boot.web.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class IndexController {
+
+    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
+    public IndexController() {
+    }
+
+    @RequestMapping(value = "/", method = GET)
+    public ModelAndView home(HttpServletRequest request) {
+        log.info("Hello, World!");
+
+        return new ModelAndView("index");
+    }
+
+}
